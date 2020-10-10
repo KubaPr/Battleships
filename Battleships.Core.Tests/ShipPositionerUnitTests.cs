21e67e1a346602ce3cc0f524877627759054009c
KubaPr <kuba.prokopiuk@gmail.com>
2020-10-10 19:41:22 +0200

    Fix positioning algorithm and improve code readability.

--- Battleships.Core.Tests/ShipPositionerUnitTests.cs
+++ Battleships.Core.Tests/ShipPositionerUnitTests.cs
@@ -1,6 +1,7 @@
 ﻿using FakeItEasy;
 using FluentAssertions;
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 
 namespace Battleships.Core.Tests
@@ -23,7 +24,7 @@ namespace Battleships.Core.Tests
             var fiveMastedShipPositions = new List<Position> { new Position(new Coordinates(0, 0)) };
 
             A.CallTo(() => randomPositionerDouble
-                .CreatePositionsForShip(A<Ship>.That.Matches(ship => ship.NumberOfMasts == 5)))
+                .GeneratePositionsForShip(A<Ship>.That.Matches(ship => ship.NumberOfMasts == 5)))
                 .Returns(fiveMastedShipPositions);
 
             _subject.CreatePositions().Should().Contain(fiveMastedShipPositions).And.HaveCount(1);
@@ -36,10 +37,10 @@ namespace Battleships.Core.Tests
             var anotherFourMastedShipPositions = new List<Position> { new Position(new Coordinates(9, 2)) };
 
             A.CallTo(() => randomPositionerDouble
-                .CreatePositionsForShip(A<Ship>.That.Matches(ship => ship.NumberOfMasts == 4)))
+                .GeneratePositionsForShip(A<Ship>.That.Matches(ship => ship.NumberOfMasts == 4)))
                 .Returns(fourMastedShipPositions).Once();
             A.CallTo(() => randomPositionerDouble
-                .CreatePositionsForShip(A<Ship>.That.Matches(ship => ship.NumberOfMasts == 4)))
+                .GeneratePositionsForShip(A<Ship>.That.Matches(ship => ship.NumberOfMasts == 4)))
                 .Returns(anotherFourMastedShipPositions).Once();
 
             _subject.CreatePositions().Should().Contain(fourMastedShipPositions)
@@ -47,20 +48,60 @@ namespace Battleships.Core.Tests
                 .And.HaveCount(2);
         }
 
-        //TODO: test cases? Or maybe it can be tested better
-        [Test]
-        public void WhenShipsAreNotAtLeastOnePositionAway_ShouldReposition()
+        [TestCase(0, 0, 0, 0)]
+        [TestCase(0, 0, 0, 1)]
+        [TestCase(0, 0, 1, 0)]
+        [TestCase(0, 0, 1, 1)]
+        [TestCase(9, 9, 9, 9)]
+        [TestCase(9, 9, 9, 8)]
+        [TestCase(9, 9, 8, 9)]
+        [TestCase(9, 9, 8, 8)]
+        public void WhenShipsAreNotAtLeastOnePositionAway_ShouldReposition(
+            byte firstPositionVertical,
+            byte firstPositionHorizontal,
+            byte secondPositionVertical,
+            byte secondPositionHorizontal)
         {
-            var positions = new List<Position> { new Position(new Coordinates(0, 0)) };
-            var tooClosePositions = new List<Position> { new Position(new Coordinates(0, 1)) };
+            const byte numberOfShipsOnTheBoard = 3;
 
-            A.CallTo(() => randomPositionerDouble.CreatePositionsForShip(A<Ship>._)).Returns(positions).Once();
-            A.CallTo(() => randomPositionerDouble.CreatePositionsForShip(A<Ship>._)).Returns(tooClosePositions).Once();
+            var positions = new List<Position> { new Position(
+                new Coordinates(firstPositionVertical, firstPositionHorizontal)) };
+            var tooClosePositions = new List<Position> { new Position(
+                new Coordinates(secondPositionVertical, secondPositionHorizontal)) };
+
+            A.CallTo(() => randomPositionerDouble.GeneratePositionsForShip(A<Ship>._)).Returns(positions).Once();
+            A.CallTo(() => randomPositionerDouble.GeneratePositionsForShip(A<Ship>._)).Returns(tooClosePositions).Once();
 
             _subject.CreatePositions();
 
-            //TODO: something about this magic number
-            A.CallTo(() => randomPositionerDouble.CreatePositionsForShip(A<Ship>._)).MustHaveHappened(4, Times.Exactly);
+            A.CallTo(() => randomPositionerDouble.GeneratePositionsForShip(A<Ship>._))
+                .MustHaveHappened(numberOfShipsOnTheBoard + 1, Times.Exactly);
          }
+
+        [TestCase(0, 0, 0, 2)]
+        [TestCase(0, 0, 2, 0)]
+        [TestCase(9, 9, 9, 7)]
+        [TestCase(9, 9, 7, 9)]
+        public void WhenShipsAreAtLeastOnePositionAway_ShouldNotReposition(
+            byte firstPositionVertical,
+            byte firstPositionHorizontal,
+            byte secondPositionVertical,
+            byte secondPositionHorizontal)
+        {
+            const byte numberOfShipsOnTheBoard = 3;
+
+            var positions = new List<Position> { new Position(
+                new Coordinates(firstPositionVertical, firstPositionHorizontal)) };
+            var tooClosePositions = new List<Position> { new Position(
+                new Coordinates(secondPositionVertical, secondPositionHorizontal)) };
+
+            A.CallTo(() => randomPositionerDouble.GeneratePositionsForShip(A<Ship>._)).Returns(positions).Once();
+            A.CallTo(() => randomPositionerDouble.GeneratePositionsForShip(A<Ship>._)).Returns(tooClosePositions).Once();
+
+            _subject.CreatePositions();
+
+            A.CallTo(() => randomPositionerDouble.GeneratePositionsForShip(A<Ship>._))
+                .MustHaveHappened(numberOfShipsOnTheBoard, Times.Exactly);
+        }
     }
 }
